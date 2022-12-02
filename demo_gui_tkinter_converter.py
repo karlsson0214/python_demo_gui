424ca32f24f37a345fba22bcf855512adac3a2ee
Karlsson0214 <karlsson0214@gmail.com>
2022-12-02 06:10:29 +0100

    make code follow pep 8

--- demo_gui_tkinter_converter.py
+++ demo_gui_tkinter_converter.py
@@ -4,10 +4,10 @@ from tkinter import ttk
 # source: https://tkdocs.com/tutorial/firstexample.html
 
 
-def calculate(*args):
+def calculate():
     try:
         value = float(feet.get())
-        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+        meters.set(str(int(0.3048 * value * 10000.0 + 0.5)/10000.0))
     except ValueError:
         pass
 
@@ -16,18 +16,18 @@ root = Tk()
 root.title("Feet to Meters")
 
 mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.grid(column=0, row=0, sticky="N, W, E, S")
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
 feet = StringVar()
 feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(W, E))
+feet_entry.grid(column=2, row=1, sticky="W, E")
 
 meters = StringVar()
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky="W, E")
 
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky="W")
 
 ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
 ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
--- demo_gui_tkinter_converter_class.py
+++ demo_gui_tkinter_converter_class.py
@@ -12,16 +12,16 @@ class FeetToMeters:
         root.title("Feet to Meters")
 
         mainframe = ttk.Frame(root, padding="3 3 12 12")
-        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        mainframe.grid(column=0, row=0, sticky="N, W, E, S")
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
 
         self.feet = StringVar()
         feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.feet)
-        feet_entry.grid(column=2, row=1, sticky=(W, E))
+        feet_entry.grid(column=2, row=1, sticky="W, E")
         self.meters = StringVar()
 
-        ttk.Label(mainframe, textvariable=self.meters).grid(column=2, row=2, sticky=(W, E))
+        ttk.Label(mainframe, textvariable=self.meters).grid(column=2, row=2, sticky="W, E")
         ttk.Button(mainframe, text="Calculate", command=self.calculate).grid(column=3, row=3, sticky=W)
 
         ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
@@ -37,11 +37,11 @@ class FeetToMeters:
     def calculate(self):
         try:
             value = float(self.feet.get())
-            self.meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+            self.meters.set(str(int(0.3048 * value * 10000.0 + 0.5)/10000.0))
         except ValueError:
             pass
 
 
-root = Tk()
-FeetToMeters(root)
-root.mainloop()
+myRoot = Tk()
+FeetToMeters(myRoot)
+myRoot.mainloop()
